ab6c0883b41e604ba401f68efb982873594932fd
Rain <Rainyan@users.noreply.github.com>
2022-10-12 00:21:10 +0300

    Update comment
    Credit the original script source instead of a link to a fork

--- weebkiller.user.js
+++ weebkiller.user.js
@@ -28,7 +28,7 @@ function log(text) {
 }
 
 function IsJapanese(text) {
-    // Credit: https://gist.github.com/ryanmcgrath/982242
+    // Credit: https://gist.github.com/sym3tri/980083
     const regex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g;
     return regex.test(text);
 }
